71caf8dc1b3ad64837e8080f0d530e570b36db05
rGudavadze <roma.gudavadze@gmail.com>
2023-01-11 23:30:44 +0400

    ADD: base app

---
+++ apps/base/models.py
@@ -0,0 +1,23 @@
+import uuid
+
+from django.db import models
+
+from apps.base.managers import BaseManager
+
+
+class BaseModel(models.Model):
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False, help_text="ID"
+    )
+    created_at = models.DateTimeField(auto_now_add=True, help_text="Created At")
+    updated_at = models.DateTimeField(auto_now=True, help_text="Updated At")
+    deleted = models.BooleanField(default=False)
+
+    objects = BaseManager()
+
+    class Meta:
+        abstract = True
+
+    def delete(self, using=None, keep_parents=False):
+        self.deleted = True
+        self.save()
