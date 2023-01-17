7cd96adeb689df8b6d5a1c1a586a86f554ca9a8e
rGudavadze <roma.gudavadze@gmail.com>
2023-01-17 18:35:22 +0400

    UPDATE: modify roommer dir to roomer

--- roomer/urls.py
+++ roomer/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from roommer.spectacular import urlpatterns as swagger_urls
+from roomer.spectacular import urlpatterns as swagger_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
