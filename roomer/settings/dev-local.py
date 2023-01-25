cd8a316eba24ced57b65644d488bbee3d3140eee
rGudavadze <roma.gudavadze@gmail.com>
2023-01-25 16:55:32 +0400

    ADD: env patterns

---
+++ roomer/settings/dev-local.py
@@ -0,0 +1,10 @@
+from roomer.settings.base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = [
+    "rest_framework.renderers.JSONRenderer",
+    "rest_framework.renderers.BrowsableAPIRenderer",
+]
