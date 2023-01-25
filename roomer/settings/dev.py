54fa740fe8f30f336535dc6ef6daf5bc6442cef7
rGudavadze <roma.gudavadze@gmail.com>
2023-01-25 13:02:14 +0400

    ADD: CSRF_TRUSTED_ORIGINS

--- roomer/settings/dev.py
+++ roomer/settings/dev.py
@@ -2,9 +2,11 @@ from roomer.settings.base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["*", "https://roomer-api-dev-6u5f43nb7a-ey.a.run.app"]
+ALLOWED_HOSTS = ["*"]
 
 REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = [
     "rest_framework.renderers.JSONRenderer",
     "rest_framework.renderers.BrowsableAPIRenderer",
 ]
+
+CSRF_TRUSTED_ORIGINS = ["https://roomer-api-dev-6u5f43nb7a-ey.a.run.app"]
